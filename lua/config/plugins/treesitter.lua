eb7871618db1af699c35217480f03e6253f21f9a
Dilan Goodwin <dilanjgoodwin@yahoo.co.uk>
2025-06-20 10:47:10 +0100

    Updated completion Removing Irrelevant Comments

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -1,17 +1,17 @@
-return{
+return {
   "nvim-treesitter/nvim-treesitter",
-  enabled=true,
---  branch="main",
-  build=":TSUpdate",
+  enabled = true,
+  --  branch="main",
+  build = ":TSUpdate",
 
-  config=function()
-    require"nvim-treesitter".setup {
-      ensure_installed={"lua", "c", "rust", "java", "perl", "haskell"},
-      auto_install=false,
-      highlight={
-        enable=true,
-        disable={},
-        additional_vim_regex_highlighting=false,
+  config = function()
+    require "nvim-treesitter".setup {
+      ensure_installed = { "lua", "c", "rust", "java", "perl", "haskell" },
+      auto_install = false,
+      highlight = {
+        enable = true,
+        disable = {},
+        additional_vim_regex_highlighting = false,
       },
     }
   end,
