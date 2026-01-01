2cb3e3eabfe9cb4b5bea70ef3b026dc65472049f
DilanGoodwin <dilanjgoodwin@yahoo.co.uk>
2026-01-01 14:53:11 +0000

    Updated treesitter to auto-install key parsers

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -5,7 +5,8 @@ return {
 
   config = function()
     require('nvim-treesitter.config').setup({
-      ensure_installed = {},
+      install_dir = vim.fn.stdpath('data') .. '/site',
+      ensure_installed = {'lua', 'c', 'rust', 'java', 'perl', 'haskell'},
       auto_install = true,
       highlight = {
         enable = true,
